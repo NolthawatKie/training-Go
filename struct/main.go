f4e7f323bd0be922e3a3f8653e7ab122f3897351
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-23 22:13:23 +0700

    method commit

--- struct/main.go
+++ struct/main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type rectangle struct {
+	w, l float64
+}
+
+// function with func signature
+func Area1(rec rectangle) float64 {
+	return rec.w * rec.l
+}
+
+// function with object signature
+func (rec rectangle) Area2() float64 {
+	return rec.w * rec.l
+}
+
 func main() {
-	fmt.Println("test")
+	rec := rectangle{
+		w: 4,
+		l: 5,
+	}
+
+	rec.w = 6
+
+	fmt.Println(rec.l)
+	fmt.Println(rec.w)
+	fmt.Println(Area1(rec))
+	fmt.Println(rec.Area2())
 }
