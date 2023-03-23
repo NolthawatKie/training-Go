81476c253fd2f198cfc68d842cf486273a9bebfc
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-24 00:23:24 +0700

    Update main.go

--- interface2/main.go
+++ interface2/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/NolthawatKie/training-go/interface2/example"
+import "github.com/NolthawatKie/training-go/interface2/tree/main/example"
+
+//https://github.com/NolthawatKie/training-Go/tree/main/interface2/example
 
 func main() {
 	example.Execute()
