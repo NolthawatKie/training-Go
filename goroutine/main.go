33e09152dd9a83cbf51076fbce926c0385ae8049
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-24 10:33:49 +0700

    Update Waitgroup in main.go

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	start1 := time.Now()
-	doSomething()
-	doSomething()
-	doSomething()
-	time.Sleep(120 * time.Millisecond)
-	fmt.Printf("with no goroutine : %d\n", time.Since(start1))
+	/*
+		start1 := time.Now()
+		doSomething()
+		doSomething()
+		doSomething()
+		time.Sleep(120 * time.Millisecond)
+		fmt.Printf("with no goroutine : %d\n", time.Since(start1))
+	*/
+
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
 
 	start2 := time.Now()
-	go doSomething()
-	go doSomething()
-	go doSomething()
-	time.Sleep(120 * time.Millisecond)
+	go doSomething(wg)
+	go doSomething(wg)
+	go doSomething(wg)
+	//time.Sleep(120 * time.Millisecond)
+	wg.Wait() //Bottle neck
 	fmt.Printf("with goroutine : %d\n", time.Since(start2))
 
 }
 
-func doSomething() {
+func doSomething(wg *sync.WaitGroup) {
 	time.Sleep(100 * time.Millisecond)
 	fmt.Println("do something")
+	wg.Done()
 }
