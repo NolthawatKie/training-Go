e7a44c4e27dfe3e9e15eaabf8b0e3399f71e866f
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-24 10:46:21 +0700

    Update main.go

--- goroutine/main.go
+++ goroutine/main.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
+var i int
+var mux sync.Mutex
+
+type thing struct {
+	i   int
+	mux sync.Mutex
+}
+
 func main() {
 	/*
 		start1 := time.Now()
@@ -15,17 +23,29 @@ func main() {
 		time.Sleep(120 * time.Millisecond)
 		fmt.Printf("with no goroutine : %d\n", time.Since(start1))
 	*/
+	/*
+		wg := &sync.WaitGroup{}
+		wg.Add(3)
 
-	wg := &sync.WaitGroup{}
-	wg.Add(3)
+		start2 := time.Now()
+		go doSomething(wg)
+		go doSomething(wg)
+		go doSomething(wg)
+		//time.Sleep(120 * time.Millisecond)
+		wg.Wait() //Bottle neck
+		fmt.Printf("with goroutine : %d\n", time.Since(start2))
+	*/
 
-	start2 := time.Now()
-	go doSomething(wg)
-	go doSomething(wg)
-	go doSomething(wg)
-	//time.Sleep(120 * time.Millisecond)
-	wg.Wait() //Bottle neck
-	fmt.Printf("with goroutine : %d\n", time.Since(start2))
+	//infinity loop
+	go func() {
+		for {
+			fmt.Println(read())
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		write(i)
+	}
 
 }
 
@@ -33,4 +53,17 @@ func doSomething(wg *sync.WaitGroup) {
 	time.Sleep(100 * time.Millisecond)
 	fmt.Println("do something")
 	wg.Done()
+}
+
+func write(n int) {
+	mux.Lock()
+	defer mux.Unlock()
+	i = n
+
+}
+
+func read() int {
+	mux.Lock()
+	defer mux.Unlock()
+	return i
 }
